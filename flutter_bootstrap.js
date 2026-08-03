76b4272d689a8d8bb54e09ea5f2f5ff793d0c15b
Monaco Web Builder <monaco-web-builder@users.noreply.github.com>
2026-08-03 20:18:07 +0200

    Build web with local CanvasKit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,11 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8dea","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8dea","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}],"useLocalCanvasKit":true};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2612987695"
+    serviceWorkerVersion: "621781848"
   }
 });
