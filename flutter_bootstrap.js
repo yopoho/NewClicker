cd255f78202c18fbdf005441c5ee0387cae4a543
Monaco Web Builder <monaco-web-builder@users.noreply.github.com>
2026-08-11 17:39:17 +0200

    Self-host complete Flutter fallback fonts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: 'assets/font_fallback/',
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1048968524",
+    serviceWorkerVersion: "2683590881",
   },
 });
