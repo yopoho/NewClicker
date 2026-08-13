f12bddb2a8fd1bbc7156b9d93d14fe5fa91d0862
Monaco Web Builder <monaco-web-builder@users.noreply.github.com>
2026-08-13 14:32:50 +0200

    Update falling chips schedule and rewards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: 'assets/font_fallback/',
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3120063888",
+    serviceWorkerVersion: "3768412650",
   },
 });
