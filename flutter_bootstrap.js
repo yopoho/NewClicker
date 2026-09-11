dddfcb794a84193dc5aeb772b17304c694c2cc09
Monaco Web Builder <monaco-web-builder@users.noreply.github.com>
2026-09-11 15:48:39 +0200

    Balance roulette payout spacing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: 'assets/font_fallback/',
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2755288432",
+    serviceWorkerVersion: "2497461891",
   },
 });
