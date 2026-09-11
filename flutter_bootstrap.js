27d06cfc2de64a412e7e5b3281d449e0f7d9e16c
Monaco Web Builder <monaco-web-builder@users.noreply.github.com>
2026-09-11 15:28:52 +0200

    Align roulette result with settings

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: 'assets/font_fallback/',
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3194839441",
+    serviceWorkerVersion: "2391439718",
   },
 });
